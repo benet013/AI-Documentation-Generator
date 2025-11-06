383d3e6df71747bd46563dab46b18209a8c3f39d
Binit <benetlimbu@gmail.com>
2025-11-06 15:29:46 +0545

    changed cors origins

--- backend/readme_generator/readme_generator/deployment_settings.py
+++ backend/readme_generator/readme_generator/deployment_settings.py
@@ -21,9 +21,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://ai-documentation-generator-client.onrender.com",
+]
 
 STORAGES = {
     "default": {
